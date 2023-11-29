e3185c491f0e99f48697b548365334b73321b67a
JXChin02 <jxchin.2021@scis.smu.edu.sg>
2023-11-29 23:41:04 +0800

    adding to list of answers

--- others.js
+++ others.js
@@ -1,4 +1,4 @@
-let answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes, definitely","You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes","Don't count on it", "My reply is no","My sources say no", "Outlook not so good","Very doubtful", "Unsure, try again", "Ask again later", "Better not tell you now","Cannot predict now", "Concentrate and ask again", "Reply hazy, try again"];
+let answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes, definitely","You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes","Don't count on it", "My reply is no","My sources say no", "Outlook not so good","Very doubtful", "Unsure, try again", "Ask again later", "Better not tell you now","Cannot predict now", "Concentrate and ask again", "Reply hazy, try again", "No"];
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
